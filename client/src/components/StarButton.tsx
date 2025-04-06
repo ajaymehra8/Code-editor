8c59d1d6f1841b45ccf209b367cd46e9d89b8813
ajaymehra8 <116863739+ajaymehra8@users.noreply.github.com>
2025-04-06 12:52:32 +0530

    Snippet card & Monaco work done

---
+++ client/src/components/StarButton.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { useGlobalState } from "@/context/GlobalProvider";
+import { Star } from "lucide-react";
+
+const StarButton = ({ snippetId }: { snippetId: number }) => {
+  const { user } = useGlobalState();
+  const isStarred = false;
+  const starCount = 3;
+  // star the snippet
+  const handleStar = () => {
+    console.log(snippetId);
+  };
+  return (
+    <button
+      className={`group flex items-center gap-1.5 px-3 py-1.5 rounded-lg cursor-pointer 
+        transition-all duration-200 ${
+          isStarred
+            ? "bg-yellow-500/10 text-yellow-500 hover:bg-yellow-500/20"
+            : "bg-gray-500/10 text-gray-400 hover:bg-gray-500/20"
+        }`}
+      onClick={handleStar}
+    >
+      <Star
+        className={`w-4 h-4 ${
+          isStarred ? "fill-yellow-500" : "fill-none group-hover:fill-gray-400"
+        }`}
+      />
+      <span
+        className={`text-xs font-medium ${
+          isStarred ? "text-yellow-500" : "text-gray-400"
+        }`}
+      >
+        {starCount}
+      </span>
+    </button>
+  );
+};
+
+export default StarButton;
