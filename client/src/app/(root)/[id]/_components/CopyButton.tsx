79aa28232742a96b3b1407b8bc0c55eaa3084356
Ajay Mehra <116863739+ajaymehra8@users.noreply.github.com>
2025-04-27 17:47:39 +0530

    Some changes in designing

--- client/src/app/(root)/[id]/_components/CopyButton.tsx
+++ client/src/app/(root)/[id]/_components/CopyButton.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Check,Copy } from "lucide-react";
+import { Check, Copy } from "lucide-react";
 import React, { useState } from "react";
 
 const CopyButton = ({ code }: { code: string }) => {
@@ -14,13 +14,13 @@ const CopyButton = ({ code }: { code: string }) => {
       onClick={copyToClipboard}
       type="button"
       className="p-2 hover:bg-white/20 rounded-lg transition-all duration-200  group relative cursor-pointer"
-      
+    title="Copy code"
     >
-        {copy?(
-            <Check className="size-4 text-green-400"/>
-        ):(
-            <Copy className="size-4 text-gray-400 group:hover:text-gray-300 "/>
-        )}
+      {copy ? (
+        <Check className="size-4 text-green-400" />
+      ) : (
+        <Copy className="size-4 text-gray-400 group:hover:text-gray-300 " />
+      )}
     </button>
   );
 };
