ff7b1b3ee6c84ac3d00e52a87613ec7aab799f12
Ajay Mehra <116863739+ajaymehra8@users.noreply.github.com>
2025-06-05 22:24:56 +0530

    Authentication: Login work done

--- client/src/components/SignupModal.tsx
+++ client/src/components/SignupModal.tsx
@@ -3,7 +3,7 @@ import { Github, X } from "lucide-react";
 import { motion } from "framer-motion";
 import { useGlobalState } from "@/context/GlobalProvider";
 import { useGoogleLogin } from "@react-oauth/google";
-import { getOtp, googleLogin, signup, verifyOtp } from "../utils/api";
+import { getOtp, googleLogin, login, signup, verifyOtp } from "../utils/api";
 
 import toast from "react-hot-toast";
 import { AxiosError } from "axios";
@@ -12,17 +12,29 @@ import Image from "next/image";
 const SignupModal = () => {
   const { setOpenSignupModal, setUser, setToken } = useGlobalState();
   const [email, setEmail] = useState("");
+  const [loginEmail, setLoginEmail] = useState("");
+
   const [otp, setOTP] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
   const [step, setStep] = useState(1);
+  const [modal, setModal] = useState("login");
   const emailRef = useRef<HTMLInputElement>(null);
+  const cleanup = () => {
+    setEmail("");
+    setPassword("");
+    setOTP("");
+    setLoginEmail("");
+    setLoginPassword("");
+    setStep(1);
+  };
   // Inside the component
-useEffect(() => {
-  if (step === 1) {
-    emailRef.current?.focus();
-  }
-}, [step]);
+  useEffect(() => {
+    if (step === 1) {
+      emailRef.current?.focus();
+    }
+  }, [step]);
 
   const responseGoogle = async (authResult) => {
     try {
@@ -47,7 +59,8 @@ useEffect(() => {
     onError: responseGoogle,
     flow: "auth-code",
   });
-  const sendOtp = async () => {
+  // signup handler
+  const handleSignup = async () => {
     if (step === 1) {
       try {
         const { data } = await getOtp(email);
@@ -88,6 +101,25 @@ useEffect(() => {
       }
     }
   };
+
+  // login handler
+  const handleLogin=async()=>{
+      try {
+        const { data } = await login(loginEmail, loginPassword);
+        if (data.success) {
+          const user = JSON.stringify(data?.user);
+          localStorage.setItem("user", user);
+          localStorage.setItem("token", data.token);
+          setUser(data?.user);
+          setToken(data.token);
+          toast.success(data.message);
+          setOpenSignupModal(false);
+        }
+      } catch (err) {
+        console.log(err);
+        if (err instanceof AxiosError) toast.error(err.response?.data.message);
+      }
+  }
   return (
     <>
       <motion.div
@@ -104,118 +136,188 @@ useEffect(() => {
           className="cursor-pointer absolute right-3 top-2"
           onClick={() => setOpenSignupModal(false)}
         />
-        <h2 className="tracking-normal text-lg font-bold">
-          Sign in to Code Craft
-        </h2>
-        <p className="font-normal text-sm">
-          Start coding in your browser ! With us
-        </p>
-        {step == 1 && (
-          <div className="flex gap-[10%] w-full items-center justify-center mt-2">
-            <button className="rounded-full border py-1 px-5 cursor-pointer flex gap-3 text-[15px] items-center">
-              <Github /> Github
-            </button>
-            <button
-              className="rounded-full border py-1 px-5 cursor-pointer flex gap-3 items-center text-[15px]"
-              onClick={googleLoginHandle}
-            >
-              <Image
-                src={
-                  "https://img.icons8.com/?size=100&id=17949&format=png&color=000000"
-                }
-                alt="google-icon"
-                width={25}
-                height={10}
-              />{" "}
-              Google
-            </button>
-          </div>
-        )}
-
-        <div className="flex flex-col gap-[2px] mt-5 w-[90%]">
-          <label htmlFor="email" className="text-sm font-medium">
-            Email
-          </label>
-          <div className="relative flex items-center">
-            <input
-              type="text"
-              id="email"
-              value={email}
-              className="border w-[100%] border-black-300 rounded px-3 py-2 focus:outline-none focus:ring-0 focus:border"
-              ref={emailRef}
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
-              disabled={step != 1}
-            />
-            {step != 1 && (
-              <button
-                className="absolute right-[5%] cursor-pointer text-[blue]"
+        {modal === "signup" ? (
+          <>
+            <h2 className="tracking-normal text-lg font-bold">
+              Sign up to Code Craft
+            </h2>
+            <p className="font-normal text-sm">
+              Already have an account?{" "}
+              <span
+                className="text-[blue] cursor-pointer font-semibold"
                 onClick={() => {
-                  setStep(1);
+                  setModal("login");
+                  cleanup();
                 }}
               >
-                Edit
-              </button>
-            )}{" "}
-          </div>
-        </div>
-        {step == 2 && (
-          <div className="flex flex-col gap-[2px] mt-5 w-[90%]">
-            <label htmlFor="otp" className="text-sm font-medium">
-              Enter OTP
-            </label>
-            <input
-              type="text"
-              id="otp"
-              value={otp}
-              className="border border-black-300 rounded px-3 py-2 focus:outline-none focus:ring-0 focus:border"
-              onChange={(e) => {
-                setOTP(e.target.value);
-              }}
-            />
-          </div>
-        )}
-        {step == 3 && (
-          <>
+                Log in
+              </span>
+            </p>
+            {step == 1 && (
+              <div className="flex gap-[10%] w-full items-center justify-center mt-2">
+                <button className="rounded-full border py-1 px-5 cursor-pointer flex gap-3 text-[15px] items-center">
+                  <Github /> Github
+                </button>
+                <button
+                  className="rounded-full border py-1 px-5 cursor-pointer flex gap-3 items-center text-[15px]"
+                  onClick={googleLoginHandle}
+                >
+                  <Image
+                    src={
+                      "https://img.icons8.com/?size=100&id=17949&format=png&color=000000"
+                    }
+                    alt="google-icon"
+                    width={25}
+                    height={10}
+                  />{" "}
+                  Google
+                </button>
+              </div>
+            )}
+
             <div className="flex flex-col gap-[2px] mt-5 w-[90%]">
-              <label htmlFor="name" className="text-sm font-medium">
-                Enter your name
+              <label htmlFor="email" className="text-sm font-medium">
+                Email
               </label>
-              <input
-                type="text"
-                id="name"
-                value={name}
-                className="border border-black-300 rounded px-3 py-2 focus:outline-none focus:ring-0 focus:border"
-                onChange={(e) => {
-                  setName(e.target.value);
+              <div className="relative flex items-center">
+                <input
+                  type="text"
+                  id="email"
+                  value={email}
+                  className="border w-[100%] border-black-300 rounded px-3 py-2 focus:outline-none focus:ring-0 focus:border"
+                  ref={emailRef}
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
+                  disabled={step != 1}
+                />
+                {step != 1 && (
+                  <button
+                    className="absolute right-[5%] cursor-pointer text-[blue]"
+                    onClick={() => {
+                      setStep(1);
+                    }}
+                  >
+                    Edit
+                  </button>
+                )}{" "}
+              </div>
+            </div>
+            {step == 2 && (
+              <div className="flex flex-col gap-[2px] mt-5 w-[90%]">
+                <label htmlFor="otp" className="text-sm font-medium">
+                  Enter OTP
+                </label>
+                <input
+                  type="text"
+                  id="otp"
+                  value={otp}
+                  className="border border-black-300 rounded px-3 py-2 focus:outline-none focus:ring-0 focus:border"
+                  onChange={(e) => {
+                    setOTP(e.target.value);
+                  }}
+                />
+              </div>
+            )}
+            {step == 3 && (
+              <>
+                <div className="flex flex-col gap-[2px] mt-5 w-[90%]">
+                  <label htmlFor="name" className="text-sm font-medium">
+                    Enter your name
+                  </label>
+                  <input
+                    type="text"
+                    id="name"
+                    value={name}
+                    className="border border-black-300 rounded px-3 py-2 focus:outline-none focus:ring-0 focus:border"
+                    onChange={(e) => {
+                      setName(e.target.value);
+                    }}
+                  />
+                </div>
+                <div className="flex flex-col gap-[2px] mt-5 w-[90%]">
+                  <label htmlFor="password" className="text-sm font-medium">
+                    Create password
+                  </label>
+                  <input
+                    type="password"
+                    id="password"
+                    value={password}
+                    className="border border-black-300 rounded px-3 py-2 focus:outline-none focus:ring-0 focus:border"
+                    onChange={(e) => {
+                      setPassword(e.target.value);
+                    }}
+                  />
+                </div>
+              </>
+            )}
+            <button
+              className="mt-8 rounded-md py-2 w-[90%] bg-gradient-to-t from-gray-900 to-gray-500 cursor-pointer text-lg text-white tracking-wide"
+              onClick={handleSignup}
+            >
+              {step === 1 && "Continue"}
+              {step === 2 && "Verify OTP"}
+              {step === 3 && "Sign up"}
+            </button>
+          </>
+        ) : (
+          <>
+            <h2 className="tracking-normal text-lg font-bold">
+              Log in to Code Craft
+            </h2>
+            <p className="font-normal text-sm">
+              Don't have an account?{" "}
+              <span
+                className="text-[blue] cursor-pointer font-semibold"
+                onClick={() => {
+                  setModal("signup");
+                  cleanup();
                 }}
-              />
+              >
+                Sign up
+              </span>
+            </p>
+
+            <div className="flex flex-col gap-[2px] mt-2 w-[90%]">
+              <label htmlFor="email" className="text-sm font-medium">
+                Email
+              </label>
+              <div className="relative flex items-center">
+                <input
+                  type="text"
+                  id="email"
+                  value={loginEmail}
+                  className="border w-[100%] border-black-300 rounded px-3 py-2 focus:outline-none focus:ring-0 focus:border"
+                  onChange={(e) => {
+                    setLoginEmail(e.target.value);
+                  }}
+                />
+              </div>
             </div>
             <div className="flex flex-col gap-[2px] mt-5 w-[90%]">
               <label htmlFor="password" className="text-sm font-medium">
-                Create password
+                Your Password
               </label>
-              <input
-                type="password"
-                id="password"
-                value={password}
-                className="border border-black-300 rounded px-3 py-2 focus:outline-none focus:ring-0 focus:border"
-                onChange={(e) => {
-                  setPassword(e.target.value);
-                }}
-              />
+              <div className="relative flex items-center">
+                <input
+                  type="password"
+                  id="password"
+                  value={loginPassword}
+                  className="border w-[100%] border-black-300 rounded px-3 py-2 focus:outline-none focus:ring-0 focus:border"
+                  onChange={(e) => {
+                    setLoginPassword(e.target.value);
+                  }}
+                />
+              </div>
             </div>
+            <button
+              className="mt-8 rounded-md py-2 w-[90%] bg-gradient-to-t from-gray-900 to-gray-500 cursor-pointer text-lg text-white tracking-wide"
+              onClick={handleLogin}
+            >
+              Log in
+            </button>
           </>
         )}
-        <button
-          className="mt-8 rounded-md py-2 w-[90%] bg-gradient-to-t from-gray-900 to-gray-500 cursor-pointer text-lg text-white tracking-wide"
-          onClick={sendOtp}
-        >
-          {step === 1 && "Continue"}
-          {step === 2 && "Verify OTP"}
-          {step === 3 && "Sign up"}
-        </button>
       </motion.div>
     </>
   );
